6a550e393e743ab1be509e9723e06e7e275cd6e1
Hyper Sonic <hs@fuwar.in>
2020-06-11 16:41:25 +0200

    [APP] Close window on Escape press (any modifier OK)

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,10 @@ int main(int argc, char const *argv[])
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+            {
                 window.close();
+            }
         }
 
         window.clear();
