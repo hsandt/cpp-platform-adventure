c053b05d03a0fd4d1d1269f012cc6281d6b18c3f
Hyper Sonic <hs@fuwar.in>
2020-07-03 00:57:16 +0200

    [WARNING] Fixed logical op parentheses warning

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@ int main(int argc, char const *argv[])
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+            if (event.type == sf::Event::Closed ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
             {
                 window.close();
             }
