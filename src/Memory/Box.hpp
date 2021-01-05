582f0ade0bdac2df66d63077ecc70b15ca05727d
huulong <n.huu.long@gmail.com>
2021-01-06 00:14:42 +0100

    [BOX] Box supports move semantics

--- src/Memory/Box.hpp
+++ src/Memory/Box.hpp
@@ -5,7 +5,9 @@
 
 /// Heap-allocated container with unique ownership guaranteed to contain a value
 /// It is similar to std::unique_ptr but default-initializes content to T's
-/// own default-constructed value. T requires default constructor
+/// own default-constructed value, and is guaranteed to contain non-nullptr data.
+/// Box supports move semantics.
+/// T requires default constructor.
 template<typename T>
 // we don't require std::is_default_constructible_v<T> which would
 // require any user of Box<T> to include T's header, even inside a header
@@ -15,6 +17,9 @@ public:
     Box();
     ~Box();
 
+    Box(Box<T>&& other);
+    Box<T>& operator=(Box<T>&& other);
+
     // allow access to non-const internal value, keeping const Box
 
     T& operator*() const { return *mc_data; }
@@ -22,10 +27,10 @@ public:
 
 private:
 
-    /* Components (exceptionally private to encourage usage of * and -> directly) */
+    /* Components (exceptionally private to require usage of * and -> directly) */
 
     /// Unique pointer to internal data
-    const std::unique_ptr<T> mc_data;
+    std::unique_ptr<T> mc_data;
 };
 
 template<typename T>
@@ -37,4 +42,21 @@ Box<T>::Box() :
 template<typename T>
 Box<T>::~Box()
 {
+}
+
+template<typename T>
+Box<T>::Box(Box<T>&& other)
+{
+    mc_data = std::move(other.mc_data);
+}
+
+template<typename T>
+Box<T>& Box<T>::operator=(Box<T>&& other)
+{
+    if (this != &other)
+    {
+        mc_data = std::move(other.mc_data);
+    }
+
+    return *this;
 }
