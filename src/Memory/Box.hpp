353db837eef9b2a38d2acb4acaf277a486ead6d6
huulong <n.huu.long@gmail.com>
2021-01-10 17:04:54 +0100

    [MEMORY] Box<T> supports T constructor arguments forwarding

--- src/Memory/Box.hpp
+++ src/Memory/Box.hpp
@@ -3,18 +3,24 @@
 // std
 #include <memory>
 
-/// Heap-allocated container with unique ownership guaranteed to contain a value
-/// It is similar to std::unique_ptr but default-initializes content to T's
+/// Heap-allocated container with unique ownership guaranteed to contain a value (via a non-nullptr
+/// unique_ptr<T>)
+/// It is similar to std::unique_ptr but enforces construction of the contained object of type T.
+/// If T has a default constructor, the box also defines a default constructor relying on it.
+/// Otherwise, you need to explicitly construct your Box by passing the same arguments as you
+/// would to construct an object of type T (arguments are forwarded to std::make_unique<T>).
 /// own default-constructed value, and is guaranteed to contain non-nullptr data.
 /// Box supports move semantics.
-/// T requires default constructor.
 template<typename T>
 // we don't require std::is_default_constructible_v<T> which would
 // require any user of Box<T> to include T's header, even inside a header
 class Box
 {
 public:
-    Box();
+
+    template<typename... Args>
+    Box(Args&&... args);
+
     ~Box();
 
     Box(Box<T>&& other);
@@ -34,8 +40,9 @@ private:
 };
 
 template<typename T>
-Box<T>::Box() :
-    mc_data(std::make_unique<T>())
+template<typename... Args>
+Box<T>::Box(Args&&... args) :
+    mc_data(std::make_unique<T>(std::forward<Args>(args)...))
 {
 }
 
